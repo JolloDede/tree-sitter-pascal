284251a900ca539b5d679e1b071de65dbe8132f8
Dennis Lee <dl@aduna.ch>
2025-12-03 13:25:27 +0100

    rename

--- bindings/go/binding_test.go
+++ bindings/go/binding_test.go
@@ -3,8 +3,8 @@ package tree_sitter_pascal_test
 import (
 	"testing"
 
-	tree_sitter "github.com/smacker/go-tree-sitter"
-	"github.com/tree-sitter/tree-sitter-pascal"
+	tree_sitter_pascal "github.com/jollodede/tree-sitter-pascal/bindings/go"
+	tree_sitter "github.com/tree-sitter/go-tree-sitter"
 )
 
 func TestCanLoadGrammar(t *testing.T) {
